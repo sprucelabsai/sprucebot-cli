500930bee6282387c69204d6ae4a16499268ee47
Shane McDermed <shmcdermed@gmail.com>
2017-09-07 08:56:30 -0600

    Adds docker-compose to run services & platforms

--- config/default.js
+++ config/default.js
@@ -6,9 +6,9 @@ const {
 module.exports = {
   version,
   description,
-  platform: {
-    platform: 'git@github.com:liquidg3/com-sprucebot-platform.git',
-    api: 'git@github.com:liquidg3/com-sprucebot-api.git',
-    web: 'git@github.com:liquidg3/com-sprucebot-hello.git'
+  gitUser: 'sprucelabsai',
+  repositories: {
+    web: 'com-sprucebot-hello',
+    api: 'com-sprucebot-api'
   }
 }

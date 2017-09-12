8f6a609c73e14e9f609826465ec925179b96301d
Shane McDermed <shmcdermed@gmail.com>
2017-09-12 17:59:42 -0600

    Better version bump messaging

--- bin/version-bump.js
+++ bin/version-bump.js
@@ -3,13 +3,7 @@ const {
   spawnSync
 } = require('child_process')
 
-const rev = spawnSync('git', ['rev-parse', '--abbrev-ref', 'HEAD'])
-
-const branch = rev.stdout
-  .toString()
-  .replace(/\s/g, '')
-
-if (branch === 'dev') {
+function patch () {
   console.log('Running npm version patch')
   const patch = spawnSync('npm', ['version', 'patch', '-m', '"[skip ci] version patch"'])
 
@@ -22,4 +16,18 @@ if (branch === 'dev') {
   } else {
     throw new Error(patch.stderr.toString())
   }
+}
+
+const rev = spawnSync('git', ['rev-parse', '--abbrev-ref', 'HEAD'])
+
+const branch = rev.stdout
+  .toString()
+  .replace(/\s/g, '')
+
+switch (branch) {
+  case 'dev':
+    patch()
+    break
+  default:
+    console.log('Version bump was ignored on branch %s', branch)
 }

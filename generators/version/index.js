fb2db863f867256d474baabd399573625cbea962
Shane McDermed <shmcdermed@gmail.com>
2017-09-18 12:04:04 -0600

    SB-84 - Allow user to select version --selet-version

---
+++ generators/version/index.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const chalk = require('chalk')
+const Generator = require('yeoman-generator')
+const config = require('config')
+const Git = require('nodegit')
+
+module.exports = class extends Generator {
+  initializing () {
+    this.sourceRoot(path.join(__dirname, 'templates'))
+    this.composeWith(require.resolve('../base'), this.options)
+  }
+
+  configuring () {
+    this.promptValues = this.config.get('promptValues')
+    this.destinationRoot(this.promptValues.path)
+  }
+
+  async writing () {
+    const repositoriesConfig = config.get('repositories')
+    const repositories = Object.keys(config.get('repositories')).reduce((repos, repo) => ({
+      ...repos,
+      [repo]: {
+        name: repositoriesConfig[repo],
+        versions: []
+      }
+    }), {})
+    const prompts = []
+    for (let repo of Object.keys(repositories)) {
+      const repository = await Git.Repository.open(path.join(this.promptValues.path, repo))
+      const reference = await repository.getCurrentBranch()
+      const versions = await Git.Tag.list(repository)
+      repositories[repo].repository = repository
+      repositories[repo].versions = versions
+      if (!versions.length) {
+        versions.push(reference.name())
+        this.log(chalk.yellow(`Oops, the ${chalk.underline(repositories[repo].name)} repository doesn't have any available versions`))
+        this.log(chalk.yellow('I can only use the currently active branch'))
+      }
+      prompts.push({
+        type: 'list',
+        name: `${repo}Version`,
+        message: `Select the version to use for ${repositories[repo].name}`,
+        choices: versions
+      })
+    }
+
+    return this.prompt(prompts).then(async (answers) => {
+      for (let repo of Object.keys(repositories)) {
+        this.log(`Checkout out ${repositories[repo].name}:${answers[`${repo}Version`]}...`)
+        await repositories[repo].repository.checkoutBranch(answers[`${repo}Version`])
+        this.log(`Finished ${repositories[repo].name}:${answers[`${repo}Version`]}`)
+      }
+    })
+  }
+}
